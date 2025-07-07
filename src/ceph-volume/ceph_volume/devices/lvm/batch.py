e4844f5b58c22b22db612f19b5725b70c004765f
zer0def <zer0def@zer0def.0>
2025-07-07 13:15:38 +0200

    Revert "ceph-volume batch: reject partitions in argparser"
    This reverts commit 9742efa907aa54b3135f5daf73080b7be12534eb, PR 38156, issue 47966.
    
    Fundamentally, this changeset addresses user error, not necessarily
    improper functionality. Enable partition use.

--- src/ceph-volume/ceph_volume/devices/lvm/batch.py
+++ src/ceph-volume/ceph_volume/devices/lvm/batch.py
@@ -37,7 +37,7 @@ def separate_devices_from_lvs(devices: List[device.Device]) -> Tuple[List[device
     phys = []
     lvm = []
     for d in devices:
-        phys.append(d) if d.is_device else lvm.append(d)
+        phys.append(d) if any([d.is_device, d.is_partition]) else lvm.append(d)
     return phys, lvm
 
 
@@ -151,14 +151,14 @@ class Batch(object):
         parser.add_argument(
             '--db-devices',
             nargs='*',
-            type=arg_validators.ValidBatchDevice(),
+            type=arg_validators.ValidDevice(),
             default=[],
             help='Devices to provision OSDs db volumes',
         )
         parser.add_argument(
             '--wal-devices',
             nargs='*',
-            type=arg_validators.ValidBatchDevice(),
+            type=arg_validators.ValidDevice(),
             default=[],
             help='Devices to provision OSDs wal volumes',
         )
