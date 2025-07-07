30ab3cafef6f32f83501139af2a911019d1a2790
zer0def <zer0def@zer0def.0>
2025-07-07 14:19:14 +0200

    cephadm: pass slot count to ceph-volume

--- src/python-common/ceph/deployment/translate.py
+++ src/python-common/ceph/deployment/translate.py
@@ -142,8 +142,14 @@ class to_ceph_volume(object):
                     if self.spec.block_wal_size:
                         cmd += " --block-wal-size {}".format(self.spec.block_wal_size)
 
+                    if self.spec.wal_slots not in [None, 1]:
+                        cmd += " --block-wal-slots {}".format(int(self.spec.wal_slots))
+
                     if self.spec.block_db_size:
                         cmd += " --block-db-size {}".format(self.spec.block_db_size)
+
+                    if self.spec.db_slots not in [None, 1]:
+                        cmd += " --block-db-slots {}".format(int(self.spec.db_slots))
 
                     if d != self.NO_CRUSH:
                         cmd += " --crush-device-class {}".format(d)
